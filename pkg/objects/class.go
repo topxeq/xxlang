4bb772b06a84ddacb4c430010c24cca5d7b46512
root <topget@sina.com>
2026-03-08 21:53:43 +0800

    feat(objects): add Class and Instance object types
    Add foundational types for the OOP system:
    - Class struct with Name, SuperClass, Methods, InitMethod, and Fields
    - Instance struct with Class reference and Fields map
    - Both types implement the Object interface with Type(), Inspect(), ToBool(), and HashKey()

---
+++ pkg/objects/class.go
@@ -0,0 +1,35 @@
+// pkg/objects/class.go
+package objects
+
+import (
+	"unsafe"
+)
+
+// Class represents a class definition
+type Class struct {
+	Name       string
+	SuperClass *Class
+	Methods    map[string]*CompiledFunction
+	InitMethod *CompiledFunction // constructor
+	Fields     map[string]Object // default field values
+}
+
+func (c *Class) Type() ObjectType { return ClassType }
+func (c *Class) Inspect() string  { return "class " + c.Name }
+func (c *Class) ToBool() *Bool    { return TRUE }
+func (c *Class) HashKey() HashKey {
+	return HashKey{Type: ClassType, Value: uint64(uintptr(unsafe.Pointer(c)))}
+}
+
+// Instance represents an instance of a class
+type Instance struct {
+	Class  *Class
+	Fields map[string]Object
+}
+
+func (i *Instance) Type() ObjectType { return InstanceType }
+func (i *Instance) Inspect() string  { return i.Class.Name + " instance" }
+func (i *Instance) ToBool() *Bool    { return TRUE }
+func (i *Instance) HashKey() HashKey {
+	return HashKey{Type: InstanceType, Value: uint64(uintptr(unsafe.Pointer(i)))}
+}
---
+++ pkg/objects/class_test.go
@@ -0,0 +1,35 @@
+// pkg/objects/class_test.go
+package objects
+
+import "testing"
+
+func TestClassObject(t *testing.T) {
+	class := &Class{
+		Name:       "Person",
+		SuperClass: nil,
+		Methods:    make(map[string]*CompiledFunction),
+		Fields:     make(map[string]Object),
+	}
+
+	if class.Type() != ClassType {
+		t.Errorf("expected ClassType, got %s", class.Type())
+	}
+	if class.Inspect() != "class Person" {
+		t.Errorf("expected 'class Person', got %s", class.Inspect())
+	}
+}
+
+func TestInstanceObject(t *testing.T) {
+	class := &Class{Name: "Person"}
+	instance := &Instance{
+		Class:  class,
+		Fields: map[string]Object{"name": &String{Value: "Alice"}},
+	}
+
+	if instance.Type() != InstanceType {
+		t.Errorf("expected InstanceType, got %s", instance.Type())
+	}
+	if instance.Inspect() != "Person instance" {
+		t.Errorf("expected 'Person instance', got %s", instance.Inspect())
+	}
+}
